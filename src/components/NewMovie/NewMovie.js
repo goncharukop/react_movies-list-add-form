be386a7d0e78ecc16e256c7b95504caebf8183f6
goncharukop <goncharukop@gmail.com>
2020-11-04 15:30:23 +0200

    final

--- src/components/NewMovie/NewMovie.js
+++ src/components/NewMovie/NewMovie.js
@@ -1,36 +1,22 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 
-const fields = [
-  'title',
-  'description',
-  'imgUrl',
-  'imdbUrl',
-  'imdbId',
-];
-
-const cardFields = fields.reduce((acc, name) => {
-  return {
-    ...acc,
-    [name]: '',
-  };
-}, {});
+const defaultForm = {
+  title: '',
+  description: '',
+  imgUrl: '',
+  imdbUrl: '',
+  imdbId: '',
+};
 
 export class NewMovie extends Component {
-  state = {
-    title: 's',
-    description: 's',
-    imgUrl: 's',
-    imdbUrl: 's',
-    imdbId: 's',
-  }
+  state = { ...defaultForm }
 
   handleChange = ({ target }) => {
     this.setState(prevState => ({
       ...prevState,
       [target.name]: target.value,
     }));
-    // console.log(this.state);
   };
 
   handleSubmit = (event) => {
@@ -40,24 +26,14 @@ export class NewMovie extends Component {
 
     this.props.addMovie(movie);
 
-    this.setState({
-      newMovieFields: cardFields,
-    });
+    this.setState({ ...defaultForm });
   }
 
   setDefaultForm = () => {
-    this.setState({
-      title: '',
-      description: '',
-      imgUrl: '',
-      imdbUrl: '',
-      imdbId: '',
-    });
+    this.setState({ ...defaultForm });
   };
 
   render() {
-    const { handleChange } = this;
-
     return (
       <form
         className="ui large form"
@@ -65,70 +41,21 @@ export class NewMovie extends Component {
         name="newMovie"
         method="post"
       >
-        { Object.entries(this.state).map(([fieldName, value]) => {
-          // console.log(value);
-
-          return (
-            <>
-              <label htmlFor="fieldName">{fieldName}</label>
-              <input
-                key={fieldName}
-                type="text"
-                name="fieldName"
-                value={value}
-                onChange={handleChange}
-                required
-              />
-            </>
-          );
-        })}
-
-        {/* <label htmlFor="title">Title</label>
-        <input
-          type="text"
-          name="title"
-          value={title}
-          onChange={this.handleChange}
-          required
-        />
-
-        <label htmlFor="description">Description</label>
-        <textarea
-          type="text"
-          name="description"
-          value={description}
-          onChange={this.handleChange}
-          required
-        />
-
-        <label htmlFor="imgUrl">ImgUrl</label>
-        <input
-          type="url"
-          name="imgUrl"
-          placeholder="https://example.com"
-          value={imgUrl}
-          onChange={this.handleChange}
-          required
-        />
-
-        <label htmlFor="imdbUrl">ImdbUrl</label>
-        <input
-          type="url"
-          name="imdbUrl"
-          value={imdbUrl}
-          placeholder="https://example.com"
-          onChange={this.handleChange}
-          required
-        />
-
-        <label htmlFor="imdbId">ImdbId</label>
-        <input
-          type="text"
-          name="imdbId"
-          value={imdbId}
-          onChange={this.handleChange}
-          required
-        /> */}
+        { Object.entries(this.state).map(([fieldName, value]) => (
+          <>
+            <label htmlFor="fieldName">
+              {fieldName[0].toUpperCase() + fieldName.slice(1)}
+            </label>
+            <input
+              key={fieldName}
+              type="text"
+              name={fieldName}
+              value={value}
+              onChange={this.handleChange}
+              required
+            />
+          </>
+        ))}
 
         <button
           className="ui green button"
